2ad7377808f58b37d7df4ed7e4c2d7966272ff45
itsSoham <119623626+hoesman420@users.noreply.github.com>
2024-08-23 19:31:53 +0530

    solution.py

--- solution.py
+++ solution.py
@@ -2,18 +2,18 @@ class Solution:
     def maxVowels(self, s: str, k: int) -> str:
         vowels = set("aeiou")
         ans = cnt = sum(c in vowels for c in s[:k])
-        max_start_index = 0  # To keep track of the start index of the window with the maximum vowels
+        max_start_index = 0 
         
         for i in range(k, len(s)):
             cnt += int(s[i] in vowels) - int(s[i - k] in vowels)
             if cnt > ans:
                 ans = cnt
-                max_start_index = i - k + 1  # Update the starting index of the maximum window
+                max_start_index = i - k + 1  
         
-        # Extract and return the substring with the maximum number of vowels
+       
         return s[max_start_index:max_start_index + k]
 
 # Example usage:
 sol = Solution()
 result = sol.maxVowels("aeiouia", 5)
-print(result)  # Output: "iii"
+print(result)
